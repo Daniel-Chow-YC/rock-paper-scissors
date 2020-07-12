94ad4a6282ad48f090514cac2436c01e08d6c046
danielchowyc <daniellchow@gmail.com>
2020-07-12 03:32:38 +0100

    removed old commented out code

--- main.js
+++ main.js
@@ -19,63 +19,6 @@ function titleCase(str) {
   return str.join(' ');
 }
 
-
-
-
-// function playRound(playerSelection, computerSelection) {
-//   let ps = playerSelection.toLowerCase();
-//   if (ps == computerSelection) {
-//     msg.textContent = `It's a Draw! You both selected ${titleCase(computerSelection)}`
-//   }
-//   else if (
-//     (ps == "rock" && computerSelection == "scissors") ||
-//     (ps == "paper" && computerSelection == "rock") ||
-//     (ps == "scissors" && computerSelection == "paper")
-//   ) {
-//     msg.textContent = `You Win! ${titleCase(ps)} beats ${titleCase(computerSelection)}`
-//   }
-//   else {
-//     msg.textContent = `You Lose! ${titleCase(computerSelection)} beats ${titleCase(ps)}`
-//   }
-// }
-
-function finalResult(win, lose, draw) {
-  if (win > lose) {
-    return "Final Result: You are the Winner!!!"
-  }
-  else if (lose > win) {
-    return "Final Result: You have lost!!!"
-  }
-  else {
-    return "Final Result: It is a Draw!!!"
-  }
-}
-
-// run game() to run the actual game in browser console
-// function game() {
-//   let win = 0;
-//   let lose = 0;
-//   let draw = 0;
-//     for (let i=0; i<5; i++) {
-//       let playerSelection = prompt("Choose rock, paper or scissors: ");
-//       let computerSelection = computerPlay();
-//       var result = playRound(playerSelection, computerSelection);
-//       console.log(result)
-//         if (result.includes("Win")) {
-//           win++
-//         }
-//         else if (result.includes("Lose")) {
-//           lose++
-//         }
-//         else {
-//           draw++
-//         }
-//     }
-//     console.log(`Wins:${win}   Losses:${lose}   Draws:${draw}`)
-//     console.log(finalResult(win, lose, draw))
-
-// }
-
 let msg = document.querySelector("#msg");
 const rockBtn = document.querySelector(".btn1"); 
 const paperBtn = document.querySelector(".btn2");
@@ -154,26 +97,4 @@ function playRound(playerSelection, computerSelection) {
     
   }
 }
-
-// winsScore.textContent = `Wins: ${win}`;
-// drawsScore.textContent = `Losses: ${lose}`;
-// lossesScore.textContent = `Draws: ${draw}`;
-
-// let win = 0;
-// let lose = 0;
-// let draw = 0;
-
-
-    
-
-
-// if (msg.textContent.includes("Win")) { 
-//    win++
-// }
-// if (msg.textContent.includes("Lose")) { 
-//    lose++
-//  }
-// if (msg.textContent.includes("Draw")) { 
-//    draw++
-//  }
 
